03c93b7b714b2855f736d91518baf965250d5e66
ronald vilorio <soccer23415@gmail.com>
2018-01-07 17:43:11 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -11,6 +11,8 @@ class InteractiveRecord
     sql = "pragma table_info('#{table_name}')"
     table_info = DB[:conn].execute(sql)
     column_names = []
+    table_info.each do |row|
+    end
     binding.pry
   end
 
