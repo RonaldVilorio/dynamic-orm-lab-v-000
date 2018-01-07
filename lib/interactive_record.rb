6f46eea92611340e50c696e7e4dce08cb3908b93
ronald vilorio <soccer23415@gmail.com>
2018-01-07 18:00:41 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,8 +2,6 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'pry'
 
-attr_accessor :id,:name,:grade
-
 class InteractiveRecord
   def self.table_name
     self.to_s.downcase.pluralize
