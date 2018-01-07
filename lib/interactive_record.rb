f0fce81dfabdc68e1cb64519038ed4fce8582208
ronald vilorio <soccer23415@gmail.com>
2018-01-07 17:53:14 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -18,7 +18,7 @@ class InteractiveRecord
     end
     column_names.compact
   end
-  def initialize(options)
+  def initialize
     # options.each do |property,value|
     #   self.send("#{property}=",value)
     # end
